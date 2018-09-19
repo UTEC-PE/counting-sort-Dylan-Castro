c0e190f4b0d52b188114fce98c29fe3ff31d00e2
unknown <dylan.castro@utec.edu.pe>
2018-09-18 20:35:23 -0500

    Terminado

--- main.cpp
+++ main.cpp
@@ -1,11 +1,9 @@
 #include <cstdlib>
 #include <iostream>
-
+#include <cstring>
 using namespace std;
-
 void print(int numbers[], int size);
 void countingSort(int numbers[], int size);
-
 int main(int argc, char *argv[]) {
     int numbers[] = {1, 4, 1, 2, 7, 5, 2};
     int size = sizeof(numbers) / sizeof(*numbers);
@@ -13,8 +11,7 @@ int main(int argc, char *argv[]) {
     print(numbers, size);
     countingSort(numbers, size);
     print(numbers, size);
-    
-    system("read");
+    system("pause");
     return EXIT_SUCCESS;
 }
 
@@ -27,4 +24,34 @@ void print(int numbers[], int size) {
 
 void countingSort(int numbers[], int size) {
     // TODO
+    int mayor=numbers[0];
+    int menor=numbers[0];
+    int temp[size];
+    //Se crea una matriz temporal con los datos de numbers, y se encuentran
+    //el numero mayor y el menor
+    for (int i = 0; i < size; i++) {
+        temp[i]=numbers[i];
+        if(numbers[i]>mayor){
+            mayor=numbers[i];
+        }
+        if(numbers[i]<menor){
+            menor=numbers[i];
+        }
+    }
+    int index[mayor+1]={};
+    //Cuenta cada elemento de la matriz numbers y guarda el nro de veces que aparece
+    for (int i = 0; i < size; i++) {
+        index[temp[i]]++;
+    }
+    //Se modifica el index sumando cada elemento con el elemento anterior(contador)
+    int contador=0;
+    for (int i = menor; i <= mayor; i++) {
+        index[i]+=contador;
+        contador=index[i];
+    }
+    //Regresa los datos ordenados a la matriz resultante
+    for (int i = 0; i < size; i++) {
+        numbers[index[temp[i]]-1]=temp[i];
+        index[temp[i]]--;
+    }
 }
